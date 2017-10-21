15d9449e70bd08cf62017d24289fb328de74596b
Matthew McDonald <matthew.mcd1031@gmail.com>
2017-10-21 05:42:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var name = "Jane"
+var name = "Susan"
 var height = "70"
-if (name === "Jane") {
+if (name === "Susan") {
   console.log("The name is correct")
 } else {
   console.log ("Expected the name to be Susan")
