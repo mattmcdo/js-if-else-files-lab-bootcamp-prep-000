3da487617096ed41a0e861cd86caefa14ad26b2c
Matthew McDonald <matthew.mcd1031@gmail.com>
2017-10-21 05:32:21 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,9 +1,9 @@
 var index = require("./index.js")
 
-if (index.name === "Susan") {
+if (index.name === "Jane") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected: Jane, Received: "+index.name)
 }
 
 
